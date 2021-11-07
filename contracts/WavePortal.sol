39810fb79d627ad87f0a2e43ff64903963ff2743
KOBEsphinx242 <ado.coneo@gmail.com>
2021-11-06 19:34:40 -0500

    feat: Add wave, getTotalWaves, getWavers to contract and modify run.js

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,26 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    address[] wavers;
+
     constructor() {
         console.log("Here we go, Adol Smart Contract!!!");
+    }
+
+    function wave() public {
+        totalWaves++;
+        wavers.push(msg.sender);
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+
+    function getWavers() public view returns (address[] memory) {
+        console.log("Here are the wavers!");
+        return wavers;
     }
 }
