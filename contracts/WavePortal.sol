5a546c3e720d4a03da68bd7b9ca1559607f63006
KOBEsphinx242 <ado.coneo@gmail.com>
2021-11-10 19:58:33 -0500

    feat: Add random ether winner and 15 minutes wait

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,36 +6,55 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
-
-    event NewWave(address indexed from, uint256 timestamp, string message);
-
+    uint256 private seed;
     struct Wave {
         address waver;
         uint256 timestamp;
         string message;
     }
-
     Wave[] waves;
+    mapping(address => uint256) public lastWavedAt;
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
 
     constructor() payable {
         console.log("Here we go, Adol Smart Contract!!!");
+
+        //Initial seed
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(string memory _message) public {
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15m"
+        );
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves++;
         console.log("%s has waved!", msg.sender);
 
         waves.push(Wave(msg.sender, block.timestamp, _message));
 
-        emit NewWave(msg.sender, block.timestamp, _message);
+        seed = (block.difficulty + block.timestamp) % 100;
 
-        uint256 prizeAmount = 0.00001 ether;
-        require(
-            prizeAmount <= address(this).balance,
-            "Trying to withdraw more money than the contract has."
-        );
-        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-        require(success, "Failed to send prize to sender.");
+        /*
+         * Give a 50% chance that the user wins the prize.
+         */
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to send prize to sender.");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _message);
     }
 
     function getTotalWaves() public view returns (uint256) {
